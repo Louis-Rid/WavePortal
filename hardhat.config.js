bb69e677c09aba3e94d742113dc4b7f586ddf65b
Louis-Rid <131224467+Louis-Rid@users.noreply.github.com>
2023-05-17 09:36:49 -0600

    Added sepolia testnet as a network option

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,10 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const QUICKNODE_API_KEY = process.env.QUICK_NODE_API_KEY;
+const PRIVATE_ACCOUNT_KEY = process.env.PRIVATE_ACCOUNT_KEY;
+const PROD_QUICKNODE_KEY = process.env.PROD_QUICKNODE_KEY;
+
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,4 +24,16 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
     solidity: "0.8.17",
+    networks: {
+        sepolia: {
+            url: QUICKNODE_API_KEY,
+            accounts: [PRIVATE_ACCOUNT_KEY]
+        },
+        mainnet: {
+            url: PROD_QUICKNODE_KEY,
+            accounts: [PRIVATE_ACCOUNT_KEY]
+        }
+    }
 };
+
+
