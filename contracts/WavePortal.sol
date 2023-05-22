3624d6bdf36fdab912b035b17688fff6e9ea92a0
Louis-Rid <131224467+Louis-Rid@users.noreply.github.com>
2023-05-22 16:15:27 -0600

    Add message storage and eth reward for sending a message

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -4,17 +4,49 @@ pragma solidity ^0.8.17;
 import "hardhat/console.sol";
 
 contract WavePortal {
-    // State Variables
-    uint256 private totalWaves;
+    // Events
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    // Structs
+    struct Wave {
+        address wave;
+        string message;
+        uint256 timestamp;
+    }
 
     // Mappings
     mapping(address => uint256) public wavers;
 
+    // State Variables
+    uint256 private totalWaves;
+    Wave[] waves;
+
+    // Constructor
+    constructor() payable {
+        console.log("We have been constructed!");
+    }
+
     // Functions
-    function wave() public {
+    function wave(string memory _message) public {
         wavers[msg.sender] += 1;
         totalWaves += 1;
-        console.log("%s has waved!", msg.sender);
+        console.log("%s has waved and said '%s'", msg.sender, _message);
+
+        // Store wave in waves array
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        // Emit Event for wave being sent
+        emit NewWave(msg.sender, block.timestamp, _message);
+
+        // Prize for sending a message
+        uint256 prizeAmount = 0.0001 ether;
+        require(
+            prizeAmount <= address(this).balance,
+            "The contract does not have enough money for a 0.0001 ETH withdraw."
+        );
+
+        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+        require(success, "Failed to withdraw money from contract");
     }
 
     // Getters
@@ -27,5 +59,9 @@ contract WavePortal {
         address waversAddress
     ) public view returns (uint256) {
         return wavers[waversAddress];
+    }
+
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,22 +1,39 @@
 // Will deploy the WavePortal Contract and console the address it is deployed to
 const main = async () => {
-    const [owner, randomPerson] = await hre.ethers.getSigners();
-    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-    const waveContract = await waveContractFactory.deploy();
+    const {ethers} = hre;
+    const [owner, randomPerson] = await ethers.getSigners();
+    const waveContractFactory = await ethers.getContractFactory("WavePortal");
+    const waveContract = await waveContractFactory.deploy({
+        value: ethers.utils.parseEther("0.1")
+    });
     await waveContract.deployed(); // Waits for contract to be deployed
 
     console.log(`Contract deployed to ${waveContract.address}`);
     console.log(`Contract deployed by: ${owner.address}`);
 
+    // Gets contract balance
+    let contractBalance = await ethers.provider.getBalance(waveContract.address); 
+    console.log(
+        "Contract balance:",
+        ethers.utils.formatEther(contractBalance)
+    );
+
     await waveContract.getTotalWaves(); // Gets total waves and logs it to the console
 
-    const firstWaveTxn = await waveContract.wave(); // Increment totalWaves by one
+    const firstWaveTxn = await waveContract.wave('TEST'); // Increment totalWaves by one
     await firstWaveTxn.wait(1); // Wait for 1 block confirmation to make sure wave() fully ran
 
     await waveContract.getTotalWaves(); // Get total wave again and it should be one greater than before
 
+    // Tests change in balance after messages is sent
+    contractBalance = await ethers.provider.getBalance(waveContract.address); // Gets contract balance
+    console.log(
+        "Contract balance:",
+        ethers.utils.formatEther(contractBalance)
+    );
+
     // Tests another person running the wave() function
-    const secondWaveTxn = await waveContract.connect(randomPerson).wave();
+    const secondWaveTxn = await waveContract.connect(randomPerson).wave('TEST2');
     await secondWaveTxn.wait(1);
 
     await waveContract.getTotalWaves();
@@ -24,6 +41,10 @@ const main = async () => {
     // Verifys the wavers are being stored in mapping
     const waves = await waveContract.getTotalWavesForAddress(randomPerson.address);
     console.log(`${randomPerson.address} has waved ${waves} times`)
+
+    // Tests getAllWaves() function
+    let allWaves = await waveContract.getAllWaves();
+    console.log(allWaves)
 
 }
 
